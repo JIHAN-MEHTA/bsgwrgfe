4c1feeee9a27d42d07c9b8bf928a6424c6b52ae3
JIHAN-MEHTA <72984635+JIHAN-MEHTA@users.noreply.github.com>
2022-02-11 17:03:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function setup(){
+
+    canvas=createCanvas(400,400)
+    canvas.center()
+}
+
+function take_snapshot(){
+save('eredtfyguouerzhgoew4arnjkvfdnojkvfsdguibfsdwhibfewub].png')
+}
+
+function draw(){}
+function preload(){}
+
+
